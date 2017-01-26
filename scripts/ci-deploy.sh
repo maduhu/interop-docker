ede6890e4ecf526c331e39d0e6c238647fd94b10
Murthy Kakarlamudi <murthy@modusbox.com>
2017-01-25 21:48:17 -0500

    updated docker -f option during image creation

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -7,7 +7,7 @@ pushDocker() {
   local REPO=$(basename $PWD)
   docker build -t $REGISTRY$REPO --rm=false .
   docker login -u $JFROG_USERNAME -p $JFROG_PASSWORD -e $JFROG_EMAIL $REGISTRY
-  docker tag $REGISTRY$REPO":latest" $REGISTRY$REPO":$(git describe)"
+  docker tag -f $REGISTRY$REPO":latest" $REGISTRY$REPO":$(git describe)"
   docker push $REGISTRY$REPO":latest"
   docker push $REGISTRY$REPO":$(git describe)"
 }
